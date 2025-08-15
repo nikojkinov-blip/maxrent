7922c199fe8daf9ca13130365cc2018b71a7ad74
nikojkinov-blip <nikojkinov@gmail.com>
2025-08-16 03:28:08 +0400

    Update database.py

--- database.py
+++ database.py
@@ -6,16 +6,12 @@ db = SQLAlchemy()
 
 class Provider(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    chat_id = db.Column(db.String(50), unique=True, nullable=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=False)
+    chat_id = db.Column(db.String(50), unique=True)
+    username = db.Column(db.String(80))
+    password = db.Column(db.String(128))
     wallet_address = db.Column(db.String(120))
     balance = db.Column(db.Float, default=0.0)
     is_admin = db.Column(db.Boolean, default=False)
-    is_active = db.Column(db.Boolean, default=True)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-
-    accounts = db.relationship('MaxAccount', backref='provider', lazy=True)
 
 class MaxAccount(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -25,4 +21,3 @@ class MaxAccount(db.Model):
     is_rented = db.Column(db.Boolean, default=False)
     rented_until = db.Column(db.DateTime)
     provider_id = db.Column(db.Integer, db.ForeignKey('provider.id'))
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
